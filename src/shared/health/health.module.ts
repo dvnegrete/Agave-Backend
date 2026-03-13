fd55aed9f1021cbc7bb75f9f68a748d2d5570c0a
Damian Valenzuela Negrete <dvnegrete@gmail.com>
2026-03-12 20:19:12 -0600

    feat(health-check): Implementar endpoint de verificación de sistema. WakeUp

--- src/shared/health/health.module.ts
+++ src/shared/health/health.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DatabaseHealthService } from './database-health.service';
+import { HealthController } from './health.controller';
 
 @Module({
+  controllers: [HealthController],
   providers: [DatabaseHealthService],
   exports: [DatabaseHealthService],
 })
